7fc2d7a88609cf4d9c431c1236249de109a08edf
EasonC13 <gtes9950406@gmail.com>
2021-05-14 06:17:50 +0800

    add link for develop

--- src/pages/intro.vue
+++ src/pages/intro.vue
@@ -11,6 +11,14 @@
         <a href="#" class="card-link btn btn-link">我有問題</a>
         <router-link to="/start" class="card-link btn btn-primary">開始實驗</router-link>
         <router-link to="/logout" class="card-link btn btn-link">登出帳號</router-link>
+
+        <div v-if="true">
+          <router-link to="/multi_anonymous" class="card-link btn btn-link">multi_anonymous</router-link>
+          <router-link to="/single_anonymous" class="card-link btn btn-link">single_anonymous</router-link>
+          <router-link to="/single_anime" class="card-link btn btn-link">single_anime</router-link>
+          <router-link to="/single_real_boy" class="card-link btn btn-link">single_real_boy</router-link>
+          <router-link to="/single_real_girl" class="card-link btn btn-link">single_real_girl</router-link>
+        </div>
     </b-card>
     </div>
 
